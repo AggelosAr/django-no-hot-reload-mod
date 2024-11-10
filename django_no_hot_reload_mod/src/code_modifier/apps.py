a75da9b7ee9cee5fb780dbe9ffef569d10de8fb0
Aggelos <aggelosarelakis@gmail.com>
2024-11-10 22:35:41 +0200

    Update correct path

--- django_no_hot_reload_mod/src/code_modifier/apps.py
+++ django_no_hot_reload_mod/src/code_modifier/apps.py
@@ -2,4 +2,4 @@
 from django.apps import AppConfig
 
 class CodeModifierConfig(AppConfig):
-    name = 'example_package_papaggalos.code_modifier'  # Full path to your app module
+    name = 'django_no_hot_reload_mod.src.code_modifier'  # Adjusted path
